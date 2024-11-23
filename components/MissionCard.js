0fc4ea7e57458b2f72485cd4be5ff18405d4c702
Britney Paul <bpaul0122@gmail.com>
2024-11-23 20:11:21 +0100

    btn on the right side

--- components/MissionCard.js
+++ components/MissionCard.js
@@ -4,9 +4,9 @@ const MissionCard = ({ status, location, droneNumber }) => {
         <div className="flex flex-col p-5 mx-5 border-2 rounded-lg border-slate-600 bg-slate-200 w-96 h-[600px]">
             <img src="https://placehold.co/400x400" className="" />
             <p className="pt-5">{status}</p>
-            <p className="text-[#1D1B20]">{location}</p>
+            <p className="text-[#1D1B20] mt-4">{location}</p>
             <p className="text-gray-600">Drone #{droneNumber}</p>
-            <button className="rounded-full border-2 border-darkgray px-2.5 py-2.5 w-36 text-darkpurple font-medium">View insights</button>
+            <button className="rounded-full border-2 border-darkgray px-2.5 py-2.5 w-36 text-darkpurple font-medium ml-auto">View insights</button>
         </div>
     )
 }
